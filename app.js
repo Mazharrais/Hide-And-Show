207331b517a2acbd085fd7a3e800dabb7944b2e3
Mazharrais <mazharmajo72@gmail.com>
2024-07-31 12:29:49 +0500

    add code

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 
 let password = document.getElementById("password");
 
-document.getElementById("eyeIcon").addEventListener('click', showIcon);
+let eyeicon = document.getElementById("eyeIcon");
 
-function showIcon(){
+eyeicon.onclick = function (){
     if(password.type == "password"){
         password.type = "text";
+        eyeicon.src = "images/eye-open.png";
     } else{
         password.type = "password";
-
+        eyeicon.src = "images/eye-close.png";
     }
 }
